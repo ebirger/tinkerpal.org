630082c60c92f9fd7596d4b9fea82c3a2b153bdf
Eyal Birger <eyal.birger@gmail.com>
2013-08-22 06:11:20 +0300

    change default page layout

--- wp-content/themes/tinkjs/page.php
+++ wp-content/themes/tinkjs/page.php
@@ -30,7 +30,42 @@ get_header(); ?>
 
 <?php endwhile; // end of the loop. ?>
       </div><!-- /.span8 -->
-<div class="span4">
+<div class="span4"/>
+
+<div class="main-content">
+<div class="container">
+  <div class="marketing marketing-left">
+  <div class="row-fluid">
+    <div class="span4">
+      <?php
+if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-left" );
+?>
+    </div>
+    <div class="span4">
+      <?php
+if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-middle" );
+?>
+    </div>
+    <div class="span4">
+      <?php
+if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-right" );
+?>
+    </div>
+  </div>
+</div>
+</div><!-- /.marketing -->
+</div>
+<div class="home-examples">
+  <div class="container">
+    <div class="row">
+    <div class="span12">
+      <?php
+if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "examples-left" );
+?>
+</div>
+</div>
+</div>
+</div>
 </div>
 </div>
 </div>
