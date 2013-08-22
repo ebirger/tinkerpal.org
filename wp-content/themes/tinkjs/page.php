e447570291798784d9363b8b34a73994bf6aad85
Eyal Birger <eyal.birger@gmail.com>
2013-08-22 06:17:14 +0300

    [layout] change defult page layout

--- wp-content/themes/tinkjs/page.php
+++ wp-content/themes/tinkjs/page.php
@@ -15,62 +15,25 @@
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <div class="main-content">
-<!--  <div class="row">
   <div class="container">
-   <?php if ( function_exists( 'bootstrapwp_breadcrumbs' ) ) bootstrapwp_breadcrumbs(); ?>
-   </div>
-   </div> -->
-   <div class="container">
-      <header class="page-title">
-        <h1><?php the_title();?></h1>
-      </header>
+    <header class="page-title">
+      <h1><?php the_title();?></h1>
+    </header>
 
-      <div class="row content">
-	<div class="span8">
+    <div class="row content">
+      <div class="span8">
 
-<?php endwhile; // end of the loop. ?>
+        <?php endwhile; // end of the loop. ?>
       </div><!-- /.span8 -->
-<div class="span4"/>
 
-<div class="main-content">
-<div class="container">
-  <div class="marketing marketing-left">
-  <div class="row-fluid">
-    <div class="span4">
-      <?php
-if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-left" );
-?>
-    </div>
-    <div class="span4">
-      <?php
-if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-middle" );
-?>
-    </div>
-    <div class="span4">
-      <?php
-if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-right" );
-?>
+      <div class="span4"/>
+    </div> <!-- row content -->
+    <div class="row content">
+      <div class="span8">
+        <?php the_content();?>
+      </div>
     </div>
   </div>
-</div>
-</div><!-- /.marketing -->
-</div>
-<div class="home-examples">
-  <div class="container">
-    <div class="row">
-    <div class="span12">
-      <?php
-if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "examples-left" );
-?>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-
-</div>
 </div>
 
 <?php get_footer(); ?>
